ae88192d1d8dd689a386ffb355f90c0dfd10f019
Josue <ahadihjosue@gmail.com>
2024-08-20 19:06:11 +0200

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="flex flex-row items-center gap-4 bg-gradient-to-r from-[#672280] to-[#A626D3] px-8 py-6">
+      <img src="/troll-face.svg" alt="Troll Face" />
+      <h1>Meme Generator</h1>
+    </div>
+  );
+}
