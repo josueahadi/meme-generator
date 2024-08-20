70031ce5015ba5acb81f79cd649352a5d9dfcfe6
Josue <ahadihjosue@gmail.com>
2024-08-20 19:15:52 +0200

    chore: add "karla" font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
-    <div className="flex flex-row items-center gap-4 bg-gradient-to-r from-[#672280] to-[#A626D3] px-8 py-6">
-      <img src="/troll-face.svg" alt="Troll Face" />
-      <h1 className="text-white font-bold text-xl">Meme Generator</h1>
-    </div>
+    <header>
+      <div className="flex flex-row items-center gap-2 bg-gradient-to-r from-[#672280] to-[#A626D3] px-8 py-6">
+        <img className="h-full" src="/troll-face.svg" alt="Troll Face" />
+        <h1 className="text-white font-bold text-xl">Meme Generator</h1>
+      </div>
+    </header>
   );
 }
