6aa4e47436924363dcaa190ad72defa489dc4121
Josue <ahadihjosue@gmail.com>
2024-08-21 11:03:26 +0200

    chore: setup shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 function App() {
   return (
     <>
       <Header />
-      <div>App</div>
+      <Meme />
     </>
   );
 }
---
+++ src/components/Meme.tsx
@@ -0,0 +1,52 @@
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const Meme: React.FC = () => {
+  const form = useForm({
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default Meme;
